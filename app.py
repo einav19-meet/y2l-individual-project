671a291c38e8dd5378efa66ea79c96528255683e
dana18-meet <dana.ahmad.ghoul@gmail.com>
2019-01-29 09:32:19 +0200

    G

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
-from database import get_all_opinions
+from flask import Flask, render_template, request, redirect, url_for
+from database import get_all_opinions, create_opinion
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	opinions = get_all_opinions()
+	print(opinions)
 	return render_template('index.html', opinions = opinions)
 
 
@@ -12,6 +13,15 @@ def hello_world():
 def forming():
 	return render_template('finalForm.html')
 
+@app.route("/opinion-form", methods=["POST"])
+def opinion_post():
+	name = request.form["name"]
+	title = request.form["title"]
+	opinion= request.form["opinion"]
+
+	create_opinion(name,title,opinion)
+
+	return redirect(url_for("hello_world"))
 
 
 if __name__ == "__main__":
--- static/js/other.js
+++ static/js/other.js
@@ -26,3 +26,17 @@ $secondButton.on("click", function(e){
   e.preventDefault();
 });
 
+
+function run(interval, frames) {
+    var int = 1;
+    
+    function func() {
+        document.body.id = "b"+int;
+        int++;
+        if(int === frames) { int = 1; }
+    }
+    
+    var swap = window.setInterval(func, interval);
+}
+
+run(1000, 10); //milliseconds, frames
